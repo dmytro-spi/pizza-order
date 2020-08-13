1cbcaaca6496ea08150d22d7b620e0371d7a11d8
dkhekalo <dimon.messages@gmail.com>
2020-08-13 19:48:13 +0300

    add empty project reack+node

---
+++ react-backend/routes/users.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function (req, res, next) {
+  // res.send("respond with a resource");
+
+  res.json([
+    {
+      id: 1,
+      username: "dimon",
+    },
+    {
+      id: 2,
+      username: "dimon2",
+    },
+  ]);
+});
+
+module.exports = router;
